e7595a9df34792d24b8ebb7fbebbece2ddc9b850
yhidiroglu <yilmaz.hidiroglu@ozu.edu.tr>
2022-05-31 23:07:26 +0300

    change structure of project
    I need to use monolith to download links, because there are multiple bottlenect to complete donwload.
    
    a new class added to call from Main

---
+++ LinkDownloader/Downloader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LinkDownloader
+{
+    public class Downloader
+    {
+        public static ConcurrentBag<string> DownloadedUrlList = new ConcurrentBag<string>();
+        public static ConcurrentBag<string> DownloadedComponentList = new ConcurrentBag<string>();
+
+        public const string MainUrl = "https://tretton37.com";
+
+        public Downloader()
+        {
+
+        }
+
+        public async Task StartDownload(List<string> urlList, string downloadPath)
+        {
+            PrepareDownloadFolder(downloadPath);
+
+            Parallel.ForEach(urlList, new ParallelOptions { MaxDegreeOfParallelism = 3 }, async url =>
+            {
+                urlList = DownloadAndSaveLink(url, downloadPath);
+
+                if (urlList != null)
+                {
+                    await StartDownload(urlList, downloadPath);
+                }
+            });
+        }
+
+        private List<string> DownloadAndSaveLink(string url, string downloadPath)
+        {
+            string path = $@"{downloadPath}\{url.Split("://")[1]}.html"; //use this path with monolith
+
+            using (WebClient client = new WebClient())
+            {
+                //TODO: Call monolith library to download url
+                string htmlContent = client.DownloadString(url); //Download htmlcontent to find 
+
+                var urlList = FindUrls(htmlContent);
+                return urlList;
+            }
+        }
+
+        private List<string> FindUrls(string htmlContent)
+        {
+            var result = new List<string>();
+
+            var urlList = Regex.Matches(htmlContent, @"<a[^>]* href=""([^""]*)""").
+                Cast<Match>().Select(m => m.Groups[1].Value).ToList();
+
+            foreach (var url in urlList)
+            {
+                if (!DownloadedUrlList.Contains(string.Format("{0}{1}", MainUrl, url)))
+                {
+                    if (url.StartsWith("/"))
+                    {
+                        result.Add(string.Format("{0}{1}", MainUrl, url));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void PrepareDownloadFolder(string downloadPath)
+        {
+            if (!Directory.Exists(downloadPath))
+            {
+                Directory.CreateDirectory(downloadPath);
+            }
+
+            string assetsPath = string.Format("{0}/assets/i", downloadPath);
+            if (!Directory.Exists(assetsPath))
+            {
+                Directory.CreateDirectory(assetsPath);
+            }
+
+            string cssPath = string.Format("{0}/assets/css", downloadPath);
+            if (!Directory.Exists(cssPath))
+            {
+                Directory.CreateDirectory(cssPath);
+            }
+        }
+    }
+}
--- LinkDownloader/Program.cs
+++ LinkDownloader/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,50 +11,18 @@ namespace LinkDownloader
 {
     class Program
     {
-        public const string DownloadPath = @"C:\LinkDownloader\";
-        public const string MainUrl = "https://tretton37.com/";
+        public const string MainDownloadPath = @"C:\LinkDownloader\";
+        public const string MainUrl = "https://tretton37.com";
+
         static void Main(string[] args)
         {
-            if (!Directory.Exists(DownloadPath))
-                Directory.CreateDirectory(DownloadPath);
-
+            string downloadPath = string.Format("{0}Tretto37com-{1}", MainDownloadPath, DateTime.Now.ToString("yyyyMMddHHmmss"));
             var urlList = new List<string> { MainUrl };
 
-            StartDownload(urlList);
+            var _downloader = new Downloader();
+
+            _downloader.StartDownload(urlList, downloadPath).Wait();
         }
 
-        public static void StartDownload(List<string> urlList)
-        {
-            Parallel.ForEach(urlList, new ParallelOptions { MaxDegreeOfParallelism = 3 }, async url =>
-            {
-                urlList = await DownloadAndSaveLink(url);
-            });
-
-            if (urlList != null)
-            {
-                StartDownload(urlList);
-            }
-        }
-
-        public async static Task<List<string>> DownloadAndSaveLink(string url)
-        {
-            string path = $@"{DownloadPath}{url.Split("://")[1]}.html";
-            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
-            {
-                string htmlCode = await client.DownloadStringTaskAsync(url);
-                File.WriteAllText(path, htmlCode);
-
-                var urlList = FindUrls(htmlCode);
-                return urlList;
-            }
-        }
-
-        public static List<string> FindUrls(string htmlContent)
-        {
-            var urlList = Regex.Matches(htmlContent, @"<a[^>]* href=""([^""]*)""").
-                Cast<Match>().Select(m => m.Groups[1].Value).ToList();
-
-            return urlList;
-        }
     }
 }
